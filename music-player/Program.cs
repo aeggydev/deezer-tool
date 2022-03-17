48e6caa7ab9629d683dd7f846cadccf5bb4f837a
aeggydev <kucernak.a@gmail.com>
2022-03-17 16:16:16 +0100

    Use main without Program class

--- music-player/Program.cs
+++ music-player/Program.cs
@@ -2,40 +2,32 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using deezer_client;
 
-namespace music_player
-{
-    internal class Program
-    {
-        private static async Task Main(string[] args)
-        {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            var arl = Environment.GetEnvironmentVariable("arl");
-            var user = await Deezer.Login(arl);
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+var arl = Environment.GetEnvironmentVariable("arl");
+var user = await Deezer.Login(arl);
 
-            var userData = await user.MyFavorites();
-            var albums = userData.Favorites.Albums.Data.Take(1);
-            var musicDir = "home folder here";
-            var i = 1;
-            var max = 1;
-            Console.WriteLine();
-            albums.Reverse();
-            foreach (var album in albums)
-            {
-                try
-                {
-                    var albumObj = await Album.Get(album.Id, user);
-                    Console.Write($"Downloading {albumObj.ArtistName} - {albumObj.Title}");
-                    await albumObj.DownloadToDir(
-                        new DirectoryInfo($"{musicDir}/{albumObj.ArtistName} - {albumObj.Title}"),
-                        Namers.ArtistTrackTitleNamer);
-                    Console.Write($"\rDownloaded {albumObj.ArtistName} - {albumObj.Title}  -  {i++} out of {max}\n");
-                }
-                catch (Exception e) { }
-            }
-        }
+var userData = await user.MyFavorites();
+var albums = userData.Favorites.Albums.Data.Take(1);
+var musicDir = "home folder here";
+var i = 1;
+var max = 1;
+Console.WriteLine();
+albums.Reverse();
+foreach (var album in albums)
+{
+    try
+    {
+        var albumObj = await Album.Get(album.Id, user);
+        Console.Write($"Downloading {albumObj.ArtistName} - {albumObj.Title}");
+        await albumObj.DownloadToDir(
+            new DirectoryInfo($"{musicDir}/{albumObj.ArtistName} - {albumObj.Title}"),
+            Namers.ArtistTrackTitleNamer);
+        Console.Write($"\rDownloaded {albumObj.ArtistName} - {albumObj.Title}  -  {i++} out of {max}\n");
+    }
+    catch (Exception e)
+    {
     }
 }
