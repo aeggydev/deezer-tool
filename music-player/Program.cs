c87cbf821a1190fc7fcd95160a2d3e02d64df8d0
aeggydev <kucernak.a@gmail.com>
2022-03-17 18:37:58 +0100

    Remove dead code

--- music-player/Program.cs
+++ music-player/Program.cs
@@ -99,35 +99,3 @@ app.Command("search", c =>
 });
 Util.NoCommandToHelp(app);
 app.Execute(args);
-Environment.Exit(1);
-
-var stopwatch = new Stopwatch();
-stopwatch.Start();
-//var arl = Environment.GetEnvironmentVariable("arl");
-// if (arl is null)
-// {
-//     Console.WriteLine("arl not specified as environment variable");
-//     Environment.Exit(1);
-// }
-
-var userData = await user.MyFavorites();
-var albums = userData.Favorites.Albums.Data.Take(1);
-var musicDir = "home folder here";
-var i = 1;
-var max = 1;
-Console.WriteLine();
-foreach (var album in albums)
-{
-    try
-    {
-        var albumObj = await Album.Get(album.Id, user);
-        Console.Write($"Downloading {albumObj.ArtistName} - {albumObj.Title}");
-        await albumObj.DownloadToDir(
-            new DirectoryInfo($"{musicDir}/{albumObj.ArtistName} - {albumObj.Title}"),
-            Namers.ArtistTrackTitleNamer);
-        Console.Write($"\rDownloaded {albumObj.ArtistName} - {albumObj.Title}  -  {i++} out of {max}\n");
-    }
-    catch (Exception e)
-    {
-    }
-}
