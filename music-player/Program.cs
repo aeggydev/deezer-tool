5082d37ad791365f535fc7a6a5043b9efc10a785
aeggydev <kucernak.a@gmail.com>
2022-03-17 17:13:18 +0100

    Add a basic cli interface

--- music-player/Program.cs
+++ music-player/Program.cs
@@ -3,6 +3,53 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using deezer_client;
+using McMaster.Extensions.CommandLineUtils;
+using music_player;
+
+var app = new CommandLineApplication();
+app.HelpOption();
+app.Command("download", c =>
+{
+    c.HelpOption();
+    c.Description = "Download a track or an album";
+    c.Command("album", ca =>
+    {
+        ca.HelpOption();
+        var album = ca.Argument("album", "Name / ID of the album to download");
+        album.IsRequired(false, "You have to specify a name / ID");
+    });
+    c.Command("track", ct =>
+    {
+        ct.HelpOption();
+        var track = ct.Argument("track", "Name / ID of the track to download");
+        track.IsRequired(false, "You have to specify a name / ID");
+    });
+    c.Option("-o|--output <FILENAME>", "Specifies where to place the file", CommandOptionType.SingleOrNoValue);
+    Util.NoCommandToHelp(c);
+});
+app.Command("search", c =>
+{
+    c.HelpOption();
+    c.Description = "Search for something";
+    c.Command("album", ca =>
+    {
+        ca.Description = "Search for an album";
+        var album = ca.Argument("album", "Name of the album to search for");
+        album.IsRequired(false, "You have to specify a name");
+        ca.HelpOption();
+    });
+    c.Command("track", ct =>
+    {
+        ct.Description = "Search for an artist";
+        var artist = ct.Argument("track", "Name of the track to search");
+        artist.IsRequired(false, "You have to specify a name");
+        ct.HelpOption();
+    });
+    Util.NoCommandToHelp(c);
+});
+Util.NoCommandToHelp(app);
+app.Execute(args);
+Environment.Exit(1);
 
 var stopwatch = new Stopwatch();
 stopwatch.Start();
@@ -20,7 +67,6 @@ var musicDir = "home folder here";
 var i = 1;
 var max = 1;
 Console.WriteLine();
-albums.Reverse();
 foreach (var album in albums)
 {
     try
---
+++ music-player/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace music_player;
+
+public class Util
+{
+    public static void NoCommandToHelp(CommandLineApplication c)
+    {
+        c.OnExecute(() =>
+        {
+            Console.WriteLine("Specify a subcommand");
+            c.ShowHelp();
+            return 1;
+        });
+    }
+}
